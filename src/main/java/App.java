14b99100ed16c269fd5570c7f9bd253e5b5320a1
Johnny Ray Alt <johnnyrayalt@gmail.com>
2018-06-01 12:06:00 -0700

    added form and submittion process for adding a new team with a name

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -16,5 +16,13 @@ public class App {
             Map<String, Object> model = new HashMap<>();
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
+
+        post("/post/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String teamName = request.queryParams("teamName");
+            Team newTeam = new Team();
+            newTeam.setTeamName(teamName);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
