663aa926518c7b03215950e701b8a92f563170da
Johnny Ray Alt <johnnyrayalt@gmail.com>
2018-06-01 10:02:26 -0700

    team class instantiates correctly with teamName property + working test

--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -1,4 +1,13 @@
 package models;
 
 public class Team {
+    private String teamName;
+
+    public Team(String userTeamName) {
+        this.teamName = userTeamName;
+    }
+
+    public String getTeamName(String userTeamName) {
+        return this.teamName = userTeamName;
+    }
 }
--- src/test/java/models/TeamTest.java
+++ src/test/java/models/TeamTest.java
@@ -14,11 +14,28 @@ public class TeamTest {
 
     @Test
     public void teamClassInstantiatesCorrectly_isTrue() throws Exception {
-        Team team = new Team();
-        assertEquals(true, team instanceof Team);
+        Team testTeam = new Team("name");
+        assertEquals(true, testTeam instanceof Team);
     }
+
+    @Test
+    public void teamClassInstantiatesCorrectly_withTeamName() throws Exception {
+        String teamName = "name";
+        Team testTeam = setUpNewTeam(teamName);
+        assertEquals("name", testTeam.getTeamName(teamName));
+    }
+
+
 
     @After
     public void tearDown() throws Exception {
     }
+
+    public static Team setUpNewTeam(String userInput) {
+        return new Team(userInput);
+    }
+
+
+
+
 }
