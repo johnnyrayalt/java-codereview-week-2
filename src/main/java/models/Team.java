bfa74a87f3d701fc441350f7d948e0c7db03f2cb
Johnny Ray Alt <johnnyrayalt@gmail.com>
2018-06-01 10:26:47 -0700

    team class instantiates correctly with an arrayList of team members

--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -1,19 +1,28 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Team {
     private String teamName;
     private String teamDescription;
+    private List<String> teamMembers;
 
     public Team() {
         this.teamName = "";
         this.teamDescription = "";
+        this.teamMembers = new ArrayList<>();
     }
 
     public String getTeamName() {
-        return this.teamName;
+        return teamName;
     }
 
     public String getTeamDescription() {
-        return this.teamDescription;
+        return teamDescription;
+    }
+
+    public List<String> getTeamMembers() {
+        return teamMembers;
     }
 }
--- src/test/java/models/TeamTest.java
+++ src/test/java/models/TeamTest.java
@@ -4,6 +4,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class TeamTest {
@@ -28,6 +30,13 @@ public class TeamTest {
     public void teamClassInstantiatesCorrectly_withDescription() throws Exception {
         Team testTeam = setUpNewTeam();
         assertEquals("", testTeam.getTeamDescription());
+    }
+
+    @Test
+    public void teamClassInstantiatesCorrectly_withTeamMembersList() throws Exception {
+        Team testTeam = setUpNewTeam();
+        ArrayList<String> expected = new ArrayList<>();
+        assertEquals(expected, testTeam.getTeamMembers());
     }
 
     @After
